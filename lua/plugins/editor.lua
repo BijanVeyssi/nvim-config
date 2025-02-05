3b493f4f3f5693e09c46cbb4826e27470a27be33
Bijan Veyssi <bijan@veyssi.com>
2025-02-05 12:44:45 +0100

    feat: add mini.ident

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -84,6 +84,50 @@ return {
         end,
     },
     {
+        "echasnovski/mini.indentscope",
+        version = false, -- wait till new 0.7.0 release to put it back on semver
+        opts = {
+            symbol = "▎",
+            options = { try_as_border = true },
+            draw = {
+                animation = function()
+                    return 7
+                end,
+            },
+        },
+        init = function()
+            vim.api.nvim_create_autocmd("FileType", {
+                pattern = {
+                    "Trouble",
+                    "alpha",
+                    "dashboard",
+                    "fzf",
+                    "help",
+                    "lazy",
+                    "mason",
+                    "neo-tree",
+                    "notify",
+                    "snacks_dashboard",
+                    "snacks_notif",
+                    "snacks_terminal",
+                    "snacks_win",
+                    "toggleterm",
+                    "trouble",
+                },
+                callback = function()
+                    vim.b.miniindentscope_disable = true
+                end,
+            })
+
+            vim.api.nvim_create_autocmd("User", {
+                pattern = "SnacksDashboardOpened",
+                callback = function(data)
+                    vim.b[data.buf].miniindentscope_disable = true
+                end,
+            })
+        end,
+    },
+    {
         "f-person/git-blame.nvim",
         dependencies = {},
         keys = {
